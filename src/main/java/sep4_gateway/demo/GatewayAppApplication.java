470267bb33df69d179bfd7670fd405ad74715b30
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2022-04-21 15:13:59 +0200

    //Pascal initial commit

---
+++ src/main/java/sep4_gateway/demo/GatewayAppApplication.java
@@ -0,0 +1,13 @@
+package sep4_gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sep4_gateway/demo/GatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sep4_gateway.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
