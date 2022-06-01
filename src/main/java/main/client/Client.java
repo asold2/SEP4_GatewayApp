302b32ef149f2a21c6b9c3d768242b780e4ce933
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-06-02 01:52:10 +0200

    New link

--- src/main/java/main/client/Client.java
+++ src/main/java/main/client/Client.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+//
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
